fc7c786eba546acd5043748408460981def2064a
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-28 20:59:43 +0300

    Refactored CancelGameResultRetranslator.

--- castledice-game-server-tests/NetworkManagerTests/CancelGameResultRetranslatorTests.cs
+++ castledice-game-server-tests/NetworkManagerTests/CancelGameResultRetranslatorTests.cs
@@ -1,6 +1,6 @@
 ﻿using casltedice_events_logic.ServerToClient;
-using castledice_game_server_tests.TestImplementations;
 using castledice_game_server.NetworkManager;
+using castledice_game_server.NetworkManager.PlayersTracking;
 using castledice_riptide_dto_adapters.Extensions;
 using Moq;
 using Riptide;
@@ -9,32 +9,23 @@ namespace castledice_game_server_tests;
 
 public class CancelGameResultRetranslatorTests
 {
-    [Fact]
-    public void AcceptCancelGameResultDTO_ShouldThrowInvalidOperationException_IfPlayerIdIsAbsentInDictionary()
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    //AcceptCancelGameResultDTO should find client id by using given IPlayerClientIdProvider
+    public void AcceptCancelGameResultDTO_ShouldSendMessageWithAcceptedDTO_ToClientWithAppropriateId(int playerId, ushort clientId)
     {
-        var DTO = new CancelGameResultDTO(false, 3);
-        var messageSender = new TestMessageSenderById();
-        var retranslator = new CancelGameResultRetranslator(messageSender);
-        
-        Assert.Throws<InvalidOperationException>(() => retranslator.AcceptCancelGameResultDTO(DTO));
-    }
-
-    [Fact]
-    //AcceptCancelGameResultDTO should find client id in PlayersDictionary by using id from dto and then send message to client with this id.
-    public void AcceptCancelGameResultDTO_ShouldSendMessageWithAcceptedDTO_ToClientWithAppropriateId()
-    {
-        var playerId = 3;
-        var playerClientId = (ushort)4;
-        PlayersDictionary.Dictionary.Add(playerId, playerClientId);
         var DTO = new CancelGameResultDTO(false, playerId);
         var messageSenderMock = new Mock<IMessageSenderById>();
-        var retranslator = new CancelGameResultRetranslator(messageSenderMock.Object);
+        var clientIdProviderMock = new Mock<IPlayerClientIdProvider>();
+        clientIdProviderMock.Setup(provider => provider.GetClientIdForPlayer(playerId)).Returns(clientId);
+        var retranslator = new CancelGameResultRetranslator(messageSenderMock.Object, clientIdProviderMock.Object);
         
         retranslator.AcceptCancelGameResultDTO(DTO);
         
         //Next line verifies two things. First, that Send methods was called with appropriate id. Second, that message contains appropriate dto.
-        messageSenderMock.Verify(x => x.Send(It.Is<Message>(m => MessageContainsCancelGameResultDTO(m, DTO)), playerClientId), Times.Once);
-        PlayersDictionary.Dictionary.Clear();
+        messageSenderMock.Verify(x => x.Send(It.Is<Message>(m => MessageContainsCancelGameResultDTO(m, DTO)), clientId), Times.Once);
     }
     
     private static bool MessageContainsCancelGameResultDTO(Message message, CancelGameResultDTO dto)
--- castledice-game-server/NetworkManager/CancelGameResultRetranslator.cs
+++ castledice-game-server/NetworkManager/CancelGameResultRetranslator.cs
@@ -1,5 +1,6 @@
 ﻿using casltedice_events_logic.ServerToClient;
 using castledice_game_server.NetworkManager.DTOAccepters;
+using castledice_game_server.NetworkManager.PlayersTracking;
 using castledice_riptide_dto_adapters.Extensions;
 using Riptide;
 
@@ -8,21 +9,18 @@ namespace castledice_game_server.NetworkManager;
 public class CancelGameResultRetranslator : ICancelGameResultDTOAccepter
 {
     private readonly IMessageSenderById _messageSender;
+    private readonly IPlayerClientIdProvider _playerClientIdProvider;
 
-    public CancelGameResultRetranslator(IMessageSenderById messageSender)
+    public CancelGameResultRetranslator(IMessageSenderById messageSender, IPlayerClientIdProvider playerClientIdProvider)
     {
         _messageSender = messageSender;
+        _playerClientIdProvider = playerClientIdProvider;
     }
 
     public void AcceptCancelGameResultDTO(CancelGameResultDTO dto)
     {
         var playerId = dto.PlayerId;
-        if (!PlayersDictionary.Dictionary.ContainsKey(playerId))
-        {
-            throw new InvalidOperationException("No client id found for player with id: " + playerId);
-        }
-
-        var clientId = PlayersDictionary.Dictionary[playerId];
+        var clientId = _playerClientIdProvider.GetClientIdForPlayer(playerId);
         var message = Message.Create(MessageSendMode.Reliable, (ushort)ServerToClientMessageType.CancelGame);
         message.AddCancelGameResultDTO(dto);
         _messageSender.Send(message, clientId);
