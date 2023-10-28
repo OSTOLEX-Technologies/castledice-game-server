20408c359441cdc24d3ddc9a242efd136c5f25d8
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-28 18:25:14 +0300

    Added ServerWrapper.

---
+++ castledice-game-server/NetworkManager/RiptideWrappers/ServerWrapper.cs
@@ -0,0 +1,28 @@
+﻿using Riptide;
+
+namespace castledice_game_server.NetworkManager;
+
+public class ServerWrapper : IMessageSenderById
+{
+    private readonly Server _server;
+
+    public ServerWrapper(Server server)
+    {
+        _server = server;
+    }
+
+    public void Send(Message message, ushort clientId)
+    {
+        _server.Send(message, clientId);
+    }
+
+    public void SendToAll(Message message)
+    {
+        _server.SendToAll(message);
+    }
+
+    public void SendToAll(Message message, ushort except)
+    {
+        _server.SendToAll(message, except);
+    }
+}
