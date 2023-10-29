0ae83d98c028445f257f5e26d05d62394793e705
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-29 17:12:04 +0200

    Wrote tests for ContentSpawnersListProvider.

--- castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/BoardConfigProviders/ContentSpawnersProviders/ContentSpawnersListProvider.cs
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/BoardConfigProviders/ContentSpawnersProviders/ContentSpawnersListProvider.cs
@@ -1,6 +1,24 @@
-﻿namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.BoardConfigProviders.ContentSpawnersProviders;
+﻿using castledice_game_logic;
+using castledice_game_logic.BoardGeneration.ContentGeneration;
 
-public class ContentSpawnersListProvider
+namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.BoardConfigProviders.ContentSpawnersProviders;
+
+public class ContentSpawnersListProvider : IContentSpawnersListProvider
 {
-    
+    private readonly ICastlesSpawnerProvider _castlesSpawnerProvider;
+    private readonly ITreesSpawnerProvider _treesSpawnerProvider;
+
+    public ContentSpawnersListProvider(ICastlesSpawnerProvider castlesSpawnerProvider, ITreesSpawnerProvider treesSpawnerProvider)
+    {
+        _castlesSpawnerProvider = castlesSpawnerProvider;
+        _treesSpawnerProvider = treesSpawnerProvider;
+    }
+
+    public List<IContentSpawner> GetContentSpawners(List<Player> players)
+    {
+        var contentSpawners = new List<IContentSpawner>();
+        var castlesSpawner = _castlesSpawnerProvider.GetCastlesSpawner(players);
+        contentSpawners.Add(castlesSpawner);
+        return contentSpawners;
+    }
 }
