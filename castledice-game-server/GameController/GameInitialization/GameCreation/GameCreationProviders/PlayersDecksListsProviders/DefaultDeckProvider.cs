497370b601d385edf29c53e7010d9f23e091bb9b
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-29 15:36:12 +0200

    Wrote test for DefaultDeckProvider.

---
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/PlayersDecksListsProviders/DefaultDeckProvider.cs
@@ -0,0 +1,18 @@
+﻿using castledice_game_logic.GameObjects;
+
+namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.PlayersDecksListsProviders;
+
+public class DefaultDeckProvider : IPlayerDeckProvider
+{
+    private readonly List<PlacementType> _defaultDeck;
+
+    public DefaultDeckProvider(List<PlacementType> defaultDeck)
+    {
+        _defaultDeck = defaultDeck;
+    }
+
+    public List<PlacementType> GetDeckForPlayer(int playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
