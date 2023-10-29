6dff7562ccfc6cfc73027c5f6c13ec2e145a3fac
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-29 15:53:13 +0200

    Wrote tests for BoardConfigProvider.

--- castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/BoardConfigProviders/BoardConfigProvider.cs
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/BoardConfigProviders/BoardConfigProvider.cs
@@ -1,10 +1,20 @@
 ﻿using castledice_game_logic;
 using castledice_game_logic.GameConfiguration;
+using castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.BoardConfigProviders.ContentSpawnersProviders;
 
 namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.BoardConfigProviders;
 
 public class BoardConfigProvider : IBoardConfigProvider
 {
+    private readonly ICellsGeneratorProvider _cellsGeneratorProvider;
+    private readonly IContentSpawnersListProvider _contentSpawnersListProvider;
+
+    public BoardConfigProvider(ICellsGeneratorProvider cellsGeneratorProvider, IContentSpawnersListProvider contentSpawnersListProvider)
+    {
+        _cellsGeneratorProvider = cellsGeneratorProvider;
+        _contentSpawnersListProvider = contentSpawnersListProvider;
+    }
+
     public BoardConfig GetBoardConfig(List<Player> players)
     {
         throw new NotImplementedException();
