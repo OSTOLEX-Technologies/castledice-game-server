ecefc827161ed26598f50acef5fc23faf3b79601
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-29 15:15:27 +0200

    Tested and implemented DefaultKnightConfigProvider.

---
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/PlaceablesConfigProviders/DefaultKnightConfigProvider.cs
@@ -0,0 +1,20 @@
+﻿using castledice_game_logic.GameObjects.Configs;
+
+namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.PlaceablesConfigProviders;
+
+public class DefaultKnightConfigProvider : IKnightConfigProvider
+{
+    private readonly int _placementCost;
+    private readonly int _health;
+
+    public DefaultKnightConfigProvider(int placementCost, int health)
+    {
+        _placementCost = placementCost;
+        _health = health;
+    }
+
+    public KnightConfig GetKnightConfig()
+    {
+        return new KnightConfig(_placementCost, _health);
+    }
+}
