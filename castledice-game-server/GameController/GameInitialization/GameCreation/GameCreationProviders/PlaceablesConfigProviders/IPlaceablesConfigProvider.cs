01a0f59cf238df6cd5aa900f228c1d8afda40d4d
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-29 14:58:53 +0200

    Wrote necessary interfaces for PlaceablesConfigProvider.

--- castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/PlaceablesConfigProviders/IPlaceablesConfigProvider.cs
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/PlaceablesConfigProviders/IPlaceablesConfigProvider.cs
@@ -1,6 +1,6 @@
 ﻿using castledice_game_logic.GameConfiguration;
 
-namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders;
+namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.PlaceablesConfigProviders;
 
 public interface IPlaceablesConfigProvider
 {
---
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/PlaceablesConfigProviders/PlaceablesConfigProvider.cs
@@ -0,0 +1,11 @@
+﻿using castledice_game_logic.GameConfiguration;
+
+namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.PlaceablesConfigProviders;
+
+public class PlaceablesConfigProvider : IPlaceablesConfigProvider
+{
+    public PlaceablesConfig GetPlaceablesConfig()
+    {
+        throw new NotImplementedException();
+    }
+}
