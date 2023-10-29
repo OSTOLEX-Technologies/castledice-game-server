2315d27be9039812fba0256152f5f458a527949d
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-29 17:43:11 +0200

    Wrote test for TreesFactoryProvider.

---
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/BoardConfigProviders/ContentSpawnersProviders/TreesSpawning/TreesFactoryProvider.cs
@@ -0,0 +1,18 @@
+﻿using castledice_game_logic.GameObjects.Factories;
+
+namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.BoardConfigProviders.ContentSpawnersProviders.TreesSpawning;
+
+public class TreesFactoryProvider : ITreesFactoryProvider
+{
+    private readonly ITreeConfigProvider _treeConfigProvider;
+    
+    public TreesFactoryProvider(ITreeConfigProvider treeConfigProvider)
+    {
+        _treeConfigProvider = treeConfigProvider;
+    }
+    
+    public ITreesFactory GetTreesFactory()
+    {
+        throw new NotImplementedException();
+    }
+}
