b3163c2f452d1549ef72d352420af6d9f93f8b4c
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-30 16:32:06 +0200

    Tested and implemented PlaceablesConfigDataProvider.

---
+++ castledice-game-server-tests/GameControllerTests/GameInitializationTests/GameStartDataCreationTests/PlaceablesConfigDataProviderTest.cs
@@ -0,0 +1,40 @@
+﻿using castledice_game_data_logic.ConfigsData;
+using castledice_game_logic.GameConfiguration;
+using castledice_game_logic.GameObjects.Configs;
+using castledice_game_server.GameController.GameInitialization.GameStartDataCreation.Providers;
+
+namespace castledice_game_server_tests.GameControllerTests.GameInitializationTests.GameStartDataCreationTests;
+
+public class PlaceablesConfigDataProviderTest
+{
+    [Theory]
+    [MemberData(nameof(GetPlaceablesConfigDataTestCases))]
+    public void GetPlaceablesConfigData_ShouldReturnAppropriatePlaceablesConfigData(PlaceablesConfig config,
+        PlaceablesConfigData expectedData)
+    {
+        var provider = new PlaceablesConfigDataProvider();
+        
+        var actualData = provider.GetPlaceablesConfigData(config);
+        
+        Assert.Equal(expectedData, actualData);
+    }
+
+    public static IEnumerable<object[]> GetPlaceablesConfigDataTestCases()
+    {
+        yield return new object[]
+        {
+            new PlaceablesConfig(new KnightConfig(1, 2)),
+            new PlaceablesConfigData(new KnightConfigData(1, 2))
+        };
+        yield return new object[]
+        {
+            new PlaceablesConfig(new KnightConfig(3, 4)),
+            new PlaceablesConfigData(new KnightConfigData(3, 4))
+        };
+        yield return new object[]
+        {
+            new PlaceablesConfig(new KnightConfig(5, 6)),
+            new PlaceablesConfigData(new KnightConfigData(5, 6))
+        };
+    }
+}
---
+++ castledice-game-server/GameController/GameInitialization/GameStartDataCreation/Providers/PlaceablesConfigDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using castledice_game_data_logic.ConfigsData;
+using castledice_game_logic.GameConfiguration;
+using castledice_game_logic.GameObjects.Configs;
+
+namespace castledice_game_server.GameController.GameInitialization.GameStartDataCreation.Providers;
+
+public class PlaceablesConfigDataProvider : IPlaceablesConfigDataProvider
+{
+    public PlaceablesConfigData GetPlaceablesConfigData(PlaceablesConfig config)
+    {
+        return new PlaceablesConfigData(GetKnightConfigData(config.KnightConfig));
+    }
+
+    private KnightConfigData GetKnightConfigData(KnightConfig config)
+    {
+        return new KnightConfigData(config.PlacementCost, config.Health);
+    }
+}
