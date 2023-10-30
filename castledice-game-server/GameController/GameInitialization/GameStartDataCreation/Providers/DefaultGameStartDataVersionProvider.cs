99554aa676aa1a9bc21a6f7ed07fbe67bc7f8565
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-30 12:46:46 +0200

    Implemented and tested DefaultGameStartDataVersionProvider.

---
+++ castledice-game-server/GameController/GameInitialization/GameStartDataCreation/Providers/DefaultGameStartDataVersionProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace castledice_game_server.GameController.GameInitialization.GameStartDataCreation.Providers;
+
+public class DefaultGameStartDataVersionProvider : IGameStartDataVersionProvider
+{
+    private readonly string _version;
+
+    public DefaultGameStartDataVersionProvider(string version)
+    {
+        _version = version;
+    }
+
+    public string GetGameStartDataVersion()
+    {
+        return _version;
+    }
+}
