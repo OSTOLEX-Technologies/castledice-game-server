fe16a1db10f77dd811d83c30cf79c02b0bb8eaf6
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-31 12:55:49 +0200

    Tested and implemented ActiveGamesCollection.

---
+++ castledice-game-server-tests/GameControllerTests/ActiveGamesCollectionTests.cs
@@ -0,0 +1,104 @@
+﻿using castledice_game_server.GameController;
+using static castledice_game_server_tests.ObjectCreationUtility;
+
+namespace castledice_game_server_tests.GameControllerTests;
+
+public class ActiveGamesCollectionTests
+{
+    [Fact]
+    public void AddGame_ShouldThrowArgumentException_IfGameWithGivenIdAlreadyExists()
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        gamesCollection.AddGame(1, game);
+        
+        Assert.Throws<ArgumentException>(() => gamesCollection.AddGame(1, GetGame()));
+    }
+
+    [Fact]
+    public void AddGame_ShouldThrowArgumentException_IfGivenGameInstanceAlreadyExists()
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        gamesCollection.AddGame(1, game);
+        
+        Assert.Throws<ArgumentException>(() => gamesCollection.AddGame(2, game));
+    }
+
+    [Fact]
+    public void AddGame_ShouldAddGivenGame_ToCollection()
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        gamesCollection.AddGame(1, game);
+
+        Assert.Contains(game, gamesCollection);
+    }
+
+    [Fact]
+    public void GetGameId_ShouldThrowArgumentException_IfGivenGameInstanceDoesNotExists()
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        
+        Assert.Throws<ArgumentException>(() => gamesCollection.GetGameId(game));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void GetGameId_ShouldReturnIdOfAddedGame(int expectedId)
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        gamesCollection.AddGame(expectedId, game);
+        
+        var actualId = gamesCollection.GetGameId(game);
+        
+        Assert.Equal(expectedId, actualId);
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void RemoveGame_ShouldReturnFalse_IfGameWithGivenIdDoesNotExists(int id)
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        
+        var result = gamesCollection.RemoveGame(1);
+        
+        Assert.False(result);
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void RemoveGame_ShouldReturnTrue_IfGameWithGivenIdExists(int id)
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        gamesCollection.AddGame(id, game);
+        
+        var result = gamesCollection.RemoveGame(id);
+        
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void RemoveGame_ShouldRemoveGameWithGivenId_FromCollection(int id)
+    {
+        var gamesCollection = new ActiveGamesCollection();
+        var game = GetGame();
+        gamesCollection.AddGame(id, game);
+        
+        gamesCollection.RemoveGame(id);
+        
+        Assert.DoesNotContain(game, gamesCollection);
+    }
+}
--- castledice-game-server/GameController/ActiveGamesCollection.cs
+++ castledice-game-server/GameController/ActiveGamesCollection.cs
@@ -1,8 +1,46 @@
-﻿using castledice_game_logic;
+﻿using System.Collections;
+using castledice_game_logic;
 
 namespace castledice_game_server.GameController;
 
-public class ActiveGamesCollection
+public class ActiveGamesCollection : IGamesCollection
 {
-    public List<Game> ActiveGames { get; } = new();
+    private readonly Dictionary<int, Game> _games = new();
+    
+    public IEnumerator<Game> GetEnumerator()
+    {
+        return _games.Values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void AddGame(int gameId, Game game)
+    {
+        if (_games.ContainsKey(gameId))
+        {
+            throw new ArgumentException("Game with id " + gameId + " already exists");
+        }
+        if (_games.ContainsValue(game))
+        {
+            throw new ArgumentException("Given game instance already exists");
+        }
+        _games.Add(gameId, game);
+    }
+
+    public int GetGameId(Game game)
+    {
+        if (!_games.ContainsValue(game))
+        {
+            throw new ArgumentException("Given game instance does not exists");
+        }
+        return _games.First(x => x.Value == game).Key;
+    }
+
+    public bool RemoveGame(int gameId)
+    {
+        return _games.Remove(gameId);
+    }
 }
