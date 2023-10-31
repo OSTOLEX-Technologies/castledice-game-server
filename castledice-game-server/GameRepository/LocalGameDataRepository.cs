c9da252790429652554bc4bab367656949f4944f
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-31 23:39:27 +0200

    Wrote tests for LocalGameDataRepository.

---
+++ castledice-game-server/GameRepository/LocalGameDataRepository.cs
@@ -0,0 +1,21 @@
+﻿using castledice_game_data_logic;
+
+namespace castledice_game_server.GameRepository;
+
+public class LocalGameDataRepository : ILocalGameDataRepository
+{
+    public void AddGameData(GameData data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GameData GetGameData(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveGameData(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
