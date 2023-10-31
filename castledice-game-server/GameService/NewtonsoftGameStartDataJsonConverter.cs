e0571cfdb2ea50c54702a96c235f12142a7cba5a
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-31 20:56:14 +0200

    Wrote Json converter and current time provider.

---
+++ castledice-game-server/GameService/NewtonsoftGameStartDataJsonConverter.cs
@@ -0,0 +1,13 @@
+﻿using castledice_game_data_logic;
+using castledice_game_data_logic.JSONConverters;
+using Newtonsoft.Json;
+
+namespace castledice_game_server.GameService;
+
+public class NewtonsoftGameStartDataJsonConverter : IGameStartDataJsonConverter
+{
+    public string GetJson(GameStartData data)
+    {
+        return JsonConvert.SerializeObject(data, new ContentDataConverter());
+    }
+}
