97b35d19b3657e21db2b141daa13240f8ad75908
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-01 16:33:15 +0200

    Tested and implemented DataToMoveConverterProvider.

---
+++ castledice-game-server-tests/GameControllerTests/MovesControllerTests/DataToMoveConverterProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using castledice_game_data_logic.MoveConverters;
+using castledice_game_server.GameController.Moves;
+using static castledice_game_server_tests.ObjectCreationUtility;
+
+namespace castledice_game_server_tests.GameControllerTests.MovesControllerTests;
+
+public class DataToMoveConverterProviderTests
+{
+    [Fact]
+    public void GetDataToMove_ShouldReturnDataToMoveConverter_WithPlaceablesFactoryFromGame()
+    {
+        var game = GetGame();
+        var expectedFactory = game.PlaceablesFactory;
+        var dataToMoveConverterProvider = new DataToMoveConverterProvider();
+        
+        var result = dataToMoveConverterProvider.GetDataToMoveConverter(game);
+        var converter = result as DataToMoveConverter;
+        
+        Assert.Same(expectedFactory, converter.Factory);
+    }
+}
