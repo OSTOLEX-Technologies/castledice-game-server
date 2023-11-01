40800ff0e9d67a7099734ec44d9e26387aada43e
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-01 14:39:36 +0200

    Wrote necessary interfaces and MovesController stub.

---
+++ castledice-game-server/GameController/Moves/MovesController.cs
@@ -0,0 +1,27 @@
+﻿using castledice_game_data_logic.Moves;
+using castledice_game_server.Logging;
+
+namespace castledice_game_server.GameController.Moves;
+
+public class MovesController : IMovesController
+{
+    private readonly IGameForPlayerProvider _gameProvider;
+    private readonly IDataToMoveConverterProvider _converterProvider;
+    private readonly IMoveDataSender _moveDataSender;
+    private readonly IMoveStatusSender _moveStatusSender;
+    private readonly ILogger _logger;
+
+    public MovesController(IGameForPlayerProvider gameProvider, IDataToMoveConverterProvider converterProvider, IMoveDataSender moveDataSender, IMoveStatusSender moveStatusSender, ILogger logger)
+    {
+        _gameProvider = gameProvider;
+        _converterProvider = converterProvider;
+        _moveDataSender = moveDataSender;
+        _moveStatusSender = moveStatusSender;
+        _logger = logger;
+    }
+
+    public void MakeMove(MoveData moveData)
+    {
+        throw new NotImplementedException();
+    }
+}
