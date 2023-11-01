a99a8d995817a7fc03f749cee8889bef5935236f
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-01 16:48:40 +0200

    Tested and implemented MoveDataSender.

---
+++ castledice-game-server/NetworkManager/MoveDataSender.cs
@@ -0,0 +1,29 @@
+﻿using casltedice_events_logic.ServerToClient;
+using castledice_game_data_logic.Moves;
+using castledice_game_server.GameController.Moves;
+using castledice_game_server.NetworkManager.PlayersTracking;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
+
+namespace castledice_game_server.NetworkManager;
+
+public class MoveDataSender : IMoveDataSender
+{
+    private readonly IMessageSenderById _messageSender;
+    private readonly IPlayerClientIdProvider _playerClientIdProvider;
+
+    public MoveDataSender(IMessageSenderById messageSender, IPlayerClientIdProvider playerClientIdProvider)
+    {
+        _messageSender = messageSender;
+        _playerClientIdProvider = playerClientIdProvider;
+    }
+
+    public void SendDataToPlayer(MoveData moveData, int playerId)
+    {
+        var clientId = _playerClientIdProvider.GetClientIdForPlayer(playerId);
+        var message = Message.Create(MessageSendMode.Reliable, (ushort)ServerToClientMessageType.MakeMove);
+        var DTO = new MoveFromServerDTO(moveData);
+        message.AddMoveFromServerDTO(DTO);
+        _messageSender.Send(message, clientId);
+    }
+}
