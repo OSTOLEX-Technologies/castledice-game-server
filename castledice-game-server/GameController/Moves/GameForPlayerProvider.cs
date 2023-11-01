c2e1b5447edf43b15517557f62f11aabcdf66644
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-01 15:58:40 +0200

    Wrote tests for GameForPlayerProvider.

---
+++ castledice-game-server/GameController/Moves/GameForPlayerProvider.cs
@@ -0,0 +1,18 @@
+﻿using castledice_game_logic;
+
+namespace castledice_game_server.GameController.Moves;
+
+public class GameForPlayerProvider : IGameForPlayerProvider
+{
+    private readonly IGamesCollection _gamesCollection;
+
+    public GameForPlayerProvider(IGamesCollection gamesCollection)
+    {
+        _gamesCollection = gamesCollection;
+    }
+
+    public Game GetGame(int playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
