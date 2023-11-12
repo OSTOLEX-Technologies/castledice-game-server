a5270493bb2d895baa29092aa3f4bf79083ac66f
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-12 17:29:30 +0200

    Tested and implemented PlayersReadinessTracker.

---
+++ castledice-game-server-tests/GameControllerTests/PlayersReadinessTests/PlayersReadinessTrackerTests.cs
@@ -0,0 +1,31 @@
+﻿using castledice_game_server.GameController.PlayersReadiness;
+
+namespace castledice_game_server_tests.GameControllerTests.PlayersReadinessTests;
+
+public class PlayersReadinessTrackerTests
+{
+    [Theory]
+    [InlineData(1, true)]
+    [InlineData(2, false)]
+    [InlineData(1245, true)]
+    public void IsPlayerReady_ShouldReturn_IfPlayerIsReady(int playerId, bool isReady)
+    {
+        var playersReadinessTracker = new PlayersReadinessTracker();
+        playersReadinessTracker.SetPlayerReadiness(playerId, isReady);
+        
+        var result = playersReadinessTracker.IsPlayerReady(playerId);
+        
+        Assert.Equal(isReady, result);
+    }
+
+    [Fact]
+    public void IsPlayerReady_ShouldReturnFalse_IfPlayerReadinessWasNotSetBefore()
+    {
+        var playerId = 1;
+        var playersReadinessTracker = new PlayersReadinessTracker();
+        
+        var result = playersReadinessTracker.IsPlayerReady(playerId);
+        
+        Assert.False(result);
+    }
+}
---
+++ castledice-game-server/GameController/PlayersReadiness/IGamePlayersReadinessNotifier.cs
@@ -0,0 +1,9 @@
+﻿using castledice_game_logic;
+
+namespace castledice_game_server.GameController.PlayersReadiness;
+
+public interface IGamePlayersReadinessNotifier
+{
+    public event EventHandler<Game> PlayersAreReady; 
+    void NotifyPlayersAreReady(Game game);
+}
