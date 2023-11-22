c66303d2f6dc9354a512faa2a708820f1b458411
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-22 22:30:49 +0200

    Added DefaultTimeConditionConfigProvider and tested.

---
+++ castledice-game-server-tests/GameControllerTests/GameInitializationTests/GameCreationTests/GameCreationProvidersTests/TscProvidersTests/TimeConditionProvidersTests/DefaultTimeConditionConfigProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.TscProviders.TimeConditionProviders;
+
+namespace castledice_game_server_tests.GameControllerTests.GameInitializationTests.GameCreationTests.GameCreationProvidersTests.TscProvidersTests.TimeConditionProvidersTests;
+
+public class DefaultTimeConditionConfigProviderTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void GetTimeConditionConfig_ShouldReturnTimeConditionConfig_WithTurnDurationGivenInConstructor(int turnDuration)
+    {
+        var provider = new DefaultTimeConditionConfigProvider(turnDuration);
+        
+        var actualConfig = provider.GetTimeConditionConfig();
+        
+        Assert.Equal(turnDuration, actualConfig.TurnDuration);
+    }
+}
---
+++ castledice-game-server/GameController/GameInitialization/GameCreation/GameCreationProviders/TscProviders/TimeConditionProviders/DefaultTimeConditionConfigProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace castledice_game_server.GameController.GameInitialization.GameCreation.GameCreationProviders.TscProviders.TimeConditionProviders;
+
+public class DefaultTimeConditionConfigProvider : ITimeConditionConfigProvider
+{
+    private readonly int _turnDuration;
+
+    public DefaultTimeConditionConfigProvider(int turnDuration)
+    {
+        _turnDuration = turnDuration;
+    }
+
+    public TimeConditionConfig GetTimeConditionConfig()
+    {
+        return new TimeConditionConfig(_turnDuration);
+    }
+}
