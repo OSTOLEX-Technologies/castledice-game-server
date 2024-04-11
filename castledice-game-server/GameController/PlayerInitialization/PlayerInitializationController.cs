23727cdbcd1f465e5e5f00568c8b3bd7c0e0ab0a
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-04-11 13:59:13 +0300

    Removed unnecessary logging from PlayerInitializationController.

--- castledice-game-server/GameController/PlayerInitialization/PlayerInitializationController.cs
+++ castledice-game-server/GameController/PlayerInitialization/PlayerInitializationController.cs
@@ -34,7 +34,6 @@ public class PlayerInitializationController : IPlayerInitializationController
     {
         try
         {
-            _logger.Info("Initializing player with token: " + playerToken + " and clientId: " + clientId + "...");
             var playerId = await _idRetriever.RetrievePlayerIdAsync(playerToken);
             if (_playerClientIdProvider.PlayerHasClientId(playerId))
             {
@@ -42,13 +41,11 @@ public class PlayerInitializationController : IPlayerInitializationController
             }
             _playerClientIdSaver.SaveClientIdForPlayer(playerId, clientId);
             _playerInitializationResultDtoSender.SendInitializationResult(clientId, new PlayerInitializationResultDTO(true));
-            _logger.Info("Player with token: " + playerToken + " and clientId: " + clientId + " initialized successfully");
         }
         catch (Exception e)
         {
             _logger.Error(e);
             _playerInitializationResultDtoSender.SendInitializationResult(clientId, new PlayerInitializationResultDTO(false));
-            _logger.Info("Player with token: " + playerToken + " and clientId: " + clientId + " initialization failed");
         }
     }
 }
