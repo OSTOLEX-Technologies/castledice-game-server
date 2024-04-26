f4f0d368e8341a7b8e4214deb6b1c8944a9f969e
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-04-26 20:35:39 +0300

    Made players ids caching.

---
+++ castledice-game-server/Auth/CachingIdRetrieverDecorator.cs
@@ -0,0 +1,23 @@
+﻿namespace castledice_game_server.Auth;
+
+public class CachingIdRetrieverDecorator : IIdRetriever
+{
+    private readonly IIdRetriever _retriever;
+    private readonly Dictionary<string, int> _cache = new();
+
+    public CachingIdRetrieverDecorator(IIdRetriever retriever)
+    {
+        _retriever = retriever;
+    }
+
+    public async Task<int> RetrievePlayerIdAsync(string playerToken)
+    {
+        if (_cache.TryGetValue(playerToken, out var id))
+        {
+            return id;
+        }
+        id = await _retriever.RetrievePlayerIdAsync(playerToken);
+        _cache[playerToken] = id;
+        return id;        
+    }
+}
